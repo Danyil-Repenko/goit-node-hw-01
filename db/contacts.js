ea52f144384318e94a96725c2d2846cee9dc74e3
Danyil-Repenko <danyil.oficial@gmail.com>
2022-12-11 18:12:13 +0200

    try / catch

--- db/contacts.js
+++ db/contacts.js
@@ -5,42 +5,62 @@ const uniqid = require('uniqid');
 const contactPath = path.join(__dirname, 'contacts.json');
 
 async function listContacts() {
-    const result = await fs.readFile(contactPath, 'utf-8');
-    return JSON.parse(result);
+    try {
+        const result = await fs.readFile(contactPath, 'utf-8');
+        return JSON.parse(result);
+    } catch (error) {
+        return error
+    }
+
 }
 
 async function getContactById(contactId) {
-    const list = await listContacts();
-    const result = list.find(element => element.id === contactId);
-    return result || null;
+    try {
+        const list = await listContacts();
+        const result = list.find(element => element.id === contactId);
+        return result || null;
+    } catch (err) {
+        return err
+    }
+
 }
 
 async function removeContact(contactId) {
-    const list = await listContacts();
-    const index = list.findIndex(element => element.id === contactId)
-    if (index === -1) {
-        return null
+    try {
+        const list = await listContacts();
+        const index = list.findIndex(element => element.id === contactId)
+        if (index === -1) {
+            return null
+        }
+
+        const deletedContact = list.splice(index, 1)
+        await fs.writeFile(contactPath, JSON.stringify(list, null, 2));
+
+        return deletedContact;
+    } catch (err) {
+        return err;
     }
 
-    const deletedContact = list.splice(index, 1)
-    await fs.writeFile(contactPath, JSON.stringify(list, null, 2));
-
-    return deletedContact;
 
 }
 
 async function addContact({ name, email, phone }) {
-    const oldList = await listContacts();
-    const newContact = {
-        id: uniqid(),
-        name,
-        email,
-        phone,
-    };
-    const newList = [...oldList, newContact];
-    await fs.writeFile(contactPath, JSON.stringify(newList, null, 2));
+    try {
+        const oldList = await listContacts();
+        const newContact = {
+            id: uniqid(),
+            name,
+            email,
+            phone,
+        };
+        const newList = [...oldList, newContact];
+        await fs.writeFile(contactPath, JSON.stringify(newList, null, 2));
 
-    return newContact;
+        return newContact;
+    } catch (err) {
+        return err;
+    }
+
 }
 
 module.exports = {
